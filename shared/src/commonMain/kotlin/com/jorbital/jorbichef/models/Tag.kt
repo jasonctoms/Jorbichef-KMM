672d454856d782ad96dab41dd7c5b47191cc786b
Jason Toms <jasonctoms@gmail.com>
2023-04-07 14:31:13 +0200

    This commit doesnt build, graphql is annoying

--- shared/src/commonMain/kotlin/com/jorbital/jorbichef/models/Tag.kt
+++ shared/src/commonMain/kotlin/com/jorbital/jorbichef/models/Tag.kt
@@ -1,11 +1,8 @@
 package com.jorbital.jorbichef.models
 
-data class Ingredient(
+data class Tag(
     val id: String,
     val name: String,
     val resourceId: String? = null,
     val userId: String? = null,
-    val tags: List<Tag>,
 )
-
-data class IngredientInput(val name: String, val tags: List<Tag>)
--- shared/src/commonMain/kotlin/com/jorbital/jorbichef/repository/TestRepository.kt
+++ shared/src/commonMain/kotlin/com/jorbital/jorbichef/repository/TestRepository.kt
@@ -2,9 +2,9 @@ package com.jorbital.jorbichef.repository
 
 import com.jorbital.jorbichef.apollo.ApolloAPI
 import com.jorbital.jorbichef.models.Ingredient
-import com.jorbital.jorbichef.models.IngredientType
+import com.jorbital.jorbichef.type.IngredientType
 
 class TestRepository(private val apolloAPI: ApolloAPI) {
     suspend fun getIngredients() = apolloAPI.getIngredients()
-        .map { Ingredient(it.id, it.name, IngredientType.valueOf(it.type.name)) }
+        //.map { Ingredient(it.id, it.name) }
 }
